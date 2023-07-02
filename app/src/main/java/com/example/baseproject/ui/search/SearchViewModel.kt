ae3f838fe39eb7a5c81bc837b5ccfcc75c36a6a2
DoHuyHoang <dohoang225@gmail.com>
2023-07-02 17:27:28 +0700

    add search screen UI

---
+++ app/src/main/java/com/example/baseproject/ui/search/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.baseproject.ui.search
+
+import androidx.lifecycle.ViewModel
+import com.example.core.base.BaseViewModel
+
+class SearchViewModel : BaseViewModel() {
+    // TODO: Implement the ViewModel
+}
