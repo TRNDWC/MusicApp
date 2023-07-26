ab2da2b5737e1a34e88188137fa8ad80323b4935
TRNDWC <tranductdn@gmail.com>
2023-07-26 11:33:05 +0700

    add song action

--- app/src/main/java/com/example/baseproject/ui/playlist/PlaylistFragment.kt
+++ app/src/main/java/com/example/baseproject/ui/playlist/PlaylistFragment.kt
@@ -3,6 +3,7 @@ package com.example.baseproject.ui.playlist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +14,7 @@ import com.example.baseproject.databinding.FragmentPlaylistBinding
 import com.example.baseproject.navigation.AppNavigation
 import com.example.baseproject.ui.playlist.addsong.AddSongDialog
 import com.example.core.base.BaseFragment
+import com.example.core.utils.toast
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
@@ -41,13 +43,17 @@ class PlaylistFragment :
         searchAction()
 
         binding.addSong.setOnClickListener {
-            AddSongDialog().show(requireActivity().supportFragmentManager, "add_song")
+            AddSongDialog(arguments?.getParcelable<LibraryItem>("playlist")!!.playlistId).show(
+                childFragmentManager,
+                "add_song"
+            )
         }
     }
 
     override fun bindingStateView() {
         super.bindingStateView()
         val item = arguments?.getParcelable<LibraryItem>("playlist")
+        item?.let { viewModel.getSong(it.playlistId) }
 
         // material tool bar
         materialToolbar = binding.materialToolbar
@@ -64,16 +70,21 @@ class PlaylistFragment :
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
         viewModel.songList.observe(viewLifecycleOwner) { newList ->
-            if (newList != null) playlistAdapter.setFilteredList(newList)
+            if (newList != null) {
+                playlistAdapter.setFilteredList(newList)
+                mSongList = newList
+            }
         }
     }
+
     private fun searchAction() {
         binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
+
             override fun onQueryTextChange(newText: String?): Boolean {
-                playlistAdapter.setFilteredList(viewModel.filter(newText))
+                playlistAdapter.setFilteredList(viewModel.filter(newText, mSongList))
                 return true
             }
         })
